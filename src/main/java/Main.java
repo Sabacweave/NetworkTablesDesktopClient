bd4a39b351e29ac7b9ed2459a12951b1d115f1d7
Josiah Fu <josiah_fu@student.davincischools.org>
2021-02-16 19:55:06 -0800

    Attempted fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,16 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class Main {
     public static void main(String[] args) {
 
-        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        // NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        NetworkTableInstance inst = NetworkTableInstance.create();
+        inst.setServer("localhost");
         NetworkTable table = inst.getTable("photonVision");
         NetworkTableEntry poseEntry = table.getEntry("targetPose");
         inst.startClientTeam(4201);  // where TEAM=190, 294, etc, or use inst.startClient("hostname") or similar
 
-        double[] defaultValue = new double[0];
+        double[] defaultValue = {8,8,8};
+        
+        System.out.println(inst.getNetworkMode());
 
         while (true) {
             try {
@@ -19,8 +23,8 @@ public class Main {
                 System.out.println("interrupted");
                 return;
             }
-            double x = poseEntry.getDoubleArray(defaultValue)[0];
-            double y = poseEntry.getDoubleArray(defaultValue)[1];
+            double x = (poseEntry.getDoubleArray(defaultValue))[0];
+            double y = (poseEntry.getDoubleArray(defaultValue))[1];
             System.out.println("X: " + x + " Y: " + y);
         }
     }
